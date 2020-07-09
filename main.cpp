e16008d9a23d98f4556f309eeebfeee88f67dbfb
GGyul-E <kkhhss0290@gmail.com>
2020-07-08 18:37:24 -0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sum.h"
+
+int main(){
+    int s = sum(10);
+    printf("sum = %d\n", s);
+    return 0; // If we didn't put this, it's ok. it automatically make it.
+}
